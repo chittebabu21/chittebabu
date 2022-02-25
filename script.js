88d9db5157827d0c02974156d13abc2dfba0bfa0
Chittebabu <77452935+chittebabu21@users.noreply.github.com>
2022-02-25 09:18:29 +0800

    First upload
    First upload of the website files.

---
+++ script.js
@@ -0,0 +1,8 @@
+function enlargeImg(img) {
+    img.style.transform = "scale(1.5)";
+    img.style.transition = "transform 0.3s ease";
+}
+function minImg(img) {
+    img.style.transform = "scale(1)";
+    img.style.transition = "transform 0.3s ease";
+}
